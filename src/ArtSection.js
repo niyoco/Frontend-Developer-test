9b1928cfb1a71f758dccc7e61deabc794c18fce0
Marvinrose <rozzeymarvin32@gmail.com>
2022-10-16 04:27:08 +0100

    Updated the hero section

--- src/ArtSection.js
+++ src/ArtSection.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./ArtSection.css";
+import first from "./first-art-image.png";
 
 export default function ArtSection() {
   return (
     <div className="ArtSection container-fluid">
       <div className="row mt-5 mb-5">
         <div className="col-md-6 art-first-col">
-          <h2>Art in the collection</h2>
+          <h2 className="fw-bold">Art in the collection</h2>
           <p>
-            Browse a curated selection of art around the world, including <br />{" "}
-            museum exhibitions, gallery openings, upcoming and many <br /> more
+            Browse a curated selection of art around the world, including museum
+            exhibitions, gallery openings, upcoming and many more
           </p>
         </div>
         <div className="col-md-6">
@@ -18,22 +19,83 @@ export default function ArtSection() {
             type="search"
             placeholder="Search"
             autoFocus="on"
-          >
-            <i class="fa-light fa-magnifying-glass"></i>
-          </input>
+          ></input>
         </div>
       </div>
-      <div className="row">
-        <div className="col-sm-4">
-            <div className="art-blob">
-                <img className="img-fluid" alt="artimage">
-                </img>
-
-            </div>
-
+      <div className="row mb-5 art-div">
+        <div className="col-md-4">
+          <div className="art-blob">
+            <img src={first} className="img-fluid" alt="artimage"></img>
+            <h4>Plastic Hears Sculpture</h4>
+            <p className="art-date">
+              11- 12 November, 2021
+              <br />
+              Galeri Salihara, Indonesia
+            </p>
+          </div>
         </div>
-
+        <div className="col-md-4">
+          <div className="art-blob">
+            <img src={first} className="img-fluid" alt="artimage"></img>
+            <h4>Plastic Hears Sculpture</h4>
+            <p className="art-date">
+              11- 12 November, 2021
+              <br />
+              Galeri Salihara, Indonesia
+            </p>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="art-blob">
+            <img src={first} className="img-fluid" alt="artimage"></img>
+            <h4>Plastic Hears Sculpture</h4>
+            <p className="art-date">
+              11- 12 November, 2021
+              <br />
+              Galeri Salihara, Indonesia
+            </p>
+          </div>
+        </div>
       </div>
+      <div className="row mb-5">
+        <div className="col-md-4">
+          <div className="art-blob">
+            <img src={first} className="img-fluid" alt="artimage"></img>
+            <h4>Plastic Hears Sculpture</h4>
+            <p className="art-date">
+              11- 12 November, 2021
+              <br />
+              Galeri Salihara, Indonesia
+            </p>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="art-blob">
+            <img src={first} className="img-fluid" alt="artimage"></img>
+            <h4>Plastic Hears Sculpture</h4>
+            <p className="art-date">
+              11- 12 November, 2021
+              <br />
+              Galeri Salihara, Indonesia
+            </p>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="art-blob">
+            <img src={first} className="img-fluid" alt="artimage"></img>
+            <h4>Plastic Hears Sculpture</h4>
+            <p className="art-date">
+              11- 12 November, 2021
+              <br />
+              Galeri Salihara, Indonesia
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <button className="art-button mb-5 mx-auto d-block">
+        Explore more <i class="fa-regular fa-arrow-right"></i>
+      </button>
     </div>
   );
 }
--- src/Hero.js
+++ src/Hero.js
@@ -3,28 +3,25 @@ import "./hero.css";
 import image from "./hero-image.png";
 import vector from "./Vector-one.png";
 
-export default function Body() {
+export default function Hero() {
   return (
-    <div className="body container-fluid ">
+    <div className="hero container-fluid ">
       <div className="row pt-5 mb-5">
         <div className="col-md-6 first-col">
-          <h1>
-            Discover amazing <br /> art around the <br />
-            <span>world</span>{" "}
+          <h1 className="fw-bold">
+            Discover amazing art around the
+            <span> world</span>{" "}
           </h1>
           <p className="pt-4">
             Browse a curated selection of art around the world, <br /> including
             museum exhibitions, gallery openings, <br /> upcoming and many more
           </p>
-          <div className="button-div pt-5">
-            <div className="button-blob"></div>
-            <button className="button">Explore arts</button>
-          </div>
+
+          <button className="button mt-4">Explore arts</button>
         </div>
         <div className="col-md-6 mx-auto d-block second-col">
-          <div className="body-blob mx-auto d-block"></div>
           <img
-            className="img-fluid hero-image"
+            className="img-fluid hero-image mx-auto d-block"
             src={image}
             alt={image}
             width={100}
@@ -36,13 +33,6 @@ export default function Body() {
               alt={vector}
               width={106.76}
               height={100.37}
-            ></img>
-            <img
-              className="img-fluid vector-two"
-              src={vector}
-              alt={vector}
-              width={108.76}
-              height={160.37}
             ></img>
           </div>
         </div>
