0276430f69f45ccdda62bec7da5959f7376874d5
Marvinrose <rozzeymarvin32@gmail.com>
2022-10-16 06:18:53 +0100

    Installed axios

--- src/ArtSection.js
+++ src/ArtSection.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./ArtSection.css";
 import first from "./first-art-image.png";
 
 export default function ArtSection() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
+  function search() {
+    let apiKey = "14556";
+    let apiUrl = `https://api.artic.edu/api/v1/artworks?=${keyword}fields=id=${apiKey},title,artist_display,date_display,main_reference_number`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="ArtSection container-fluid">
       <div className="row mt-5 mb-5">
@@ -14,12 +36,15 @@ export default function ArtSection() {
           </p>
         </div>
         <div className="col-md-6">
-          <input
-            class="input"
-            type="search"
-            placeholder="Search"
-            autoFocus="on"
-          ></input>
+          <form onSubmit={handleSubmit}>
+            <input
+              class="input"
+              type="search"
+              placeholder="Search"
+              autoFocus="on"
+              onChange={handleKeywordChange}
+            ></input>
+          </form>
         </div>
       </div>
       <div className="row mb-5 art-div">
